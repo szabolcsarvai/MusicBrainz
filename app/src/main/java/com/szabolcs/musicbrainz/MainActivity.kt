0155126c7227639c0fd4c0f8751972596fdc01df
Szabolcs Arvai <aszabolcs@geico.com>
2019-10-09 11:02:08 +0300

    Create Android project

---
+++ app/src/main/java/com/szabolcs/musicbrainz/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.szabolcs.musicbrainz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
